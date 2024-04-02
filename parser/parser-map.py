5598643478724d1d414ca57c29e99f7825e2fbd1
ilya_popov <il_pop@mail.ru>
2024-04-02 14:16:09 +0300

    возвращаем словарь с адресом и телефоном организации

--- parser/parser-map.py
+++ parser/parser-map.py
@@ -1,6 +1,9 @@
 import time
+import selenium
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from icecream import ic
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver import Keys
@@ -8,6 +11,11 @@ from selenium.common.exceptions import NoAlertPresentException
 from tqdm import tqdm
 from itertools import product
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+url_find = "https://yandex.ru/maps/38/volgograd/chain/video/6002372/?ll=44.518709%2C48.707615&sll=44.516979%2C48.707068&sspn=0.177155%2C0.188825&z=11.97"
+url_find_1 = "https://yandex.ru/maps/org/m_video/1117900066/?ll=44.497380%2C48.749279&mode=search&sll=44.516979%2C48.707068&sspn=0.493011%2C0.188825&text=%D0%9C.%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE&z=12"
 
 
 class ParseYandexMap:
@@ -32,8 +40,7 @@ class ParseYandexMap:
         """
         element = browser.find_element(By.CLASS_NAME, "body")
         element = element.find_element(By.CLASS_NAME, "app")
-        element = element.find_element(By.CSS_SELECTOR, "div.header-view__main-layout")
-        element = element.find_element(By.TAG_NAME, "input")
+        element = element.find_element(By.CSS_SELECTOR, "input.input__control")
         element.send_keys(self.search_query)  # запись в строке поиска
 
         element = browser.find_element(By.CLASS_NAME, "body")
@@ -43,16 +50,68 @@ class ParseYandexMap:
         element = element.find_element(By.TAG_NAME, "button")
         element.click()
 
+    @staticmethod
+    def _drop_down_windows(browser):
+        """
+        список выпавших элементов по поисковому запросу
+        :param browser:
+        :return:
+        """
+        element = browser.find_element(By.CLASS_NAME, "body")
+        element = element.find_element(By.CLASS_NAME, "app")
+        element = element.find_element(By.CSS_SELECTOR, "div.scroll__container")
+        elements = element.find_elements(By.CSS_SELECTOR, "li.search-snippet-view")
+        return elements
+
+    @staticmethod
+    def _touch_element_and_get_address_phone(browser, element):
+        phone = None
+        element.click()
+        locator_address = (By.CSS_SELECTOR, "div.business-contacts-view__address")
+        target_address = WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator_address))
+        address = target_address.text
+
+        try:
+            locator_phone = (By.CSS_SELECTOR, "div.card-phones-view__more")
+            target_phone = WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator_phone))
+            target_phone.click()
+
+            locator_phone2 = (By.CSS_SELECTOR, "div.card-phones-view__phone-number")
+            target_phone2 = WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator_phone2))
+            phone = target_phone2.text
+
+        except selenium.common.exceptions.TimeoutException:
+            pass
+
+        time.sleep(3)
+        return address, phone
+
     def _parser(self):
         with webdriver.Chrome(options=self.options_chrome) as browser:
+            result = list()
+            browser.maximize_window()  # на весь экран
             browser.get(self.url)
             self._put_find(browser=browser)
-
             time.sleep(10)
+            elements = self._drop_down_windows(browser=browser)
+            for element in elements:
+                address, phone = self._touch_element_and_get_address_phone(browser=browser, element=element)
+                time.sleep(3)
+                result.append({
+                    "address": address,
+                    "phone": phone
+                })
+
+            return result
 
     def __call__(self, *args, **kwargs):
-        self._parser()
+        return self._parser()
+
+
+def mane():
+    result = ParseYandexMap(search_query="М.видео")()
+    ic(result)
 
 
 if __name__ == "__main__":
-    ParseYandexMap(search_query="М.видео")()
+    mane()
