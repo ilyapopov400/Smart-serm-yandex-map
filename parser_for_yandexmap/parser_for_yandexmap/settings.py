61b830b0723e6232360a339ead70a11b2ba274b4
ilya_popov <il_pop@mail.ru>
2024-04-03 13:56:24 +0300

    создана стартовая страница и подключены стили

--- parser_for_yandexmap/parser_for_yandexmap/settings.py
+++ parser_for_yandexmap/parser_for_yandexmap/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'parser_map.apps.ParserMapConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'parser_for_yandexmap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -121,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth.User'  # стандартная модель User
--- parser_for_yandexmap/parser_map/views.py
+++ parser_for_yandexmap/parser_map/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+
+class Index(TemplateView):
+    '''
+    стартовая страница приложения parser_map
+    '''
+    template_name = 'parser_map/index.html'
